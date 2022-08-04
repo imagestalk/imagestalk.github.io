09c2ae8e755d8be1391bcd678b7e7f4c48a27bf8
imagestalk <b-u-r-n-i-n-g-b-u-s-h@yandex.ru>
2022-08-05 01:05:43 +0300

    search bar

--- crawler/crawler.js
+++ crawler/crawler.js
@@ -19,42 +19,41 @@ function makeHttpObject() {
 function main() {
    const domain = /http[s]?:\/\/[a-z0-9.]*\.[a-z]{2,3}/g.exec(target_url);
    var request = makeHttpObject();
-
    request.open(method, target_url, true);
    request.send(null);
    request.onreadystatechange = function() {
-      console.log("readyState: " + request.readyState + "\n" + "status: " + request.status);
+      //console.log("readyState: " + request.readyState + "\n" + "status: " + request.status);
       // CORS error handling
-      if (request.status != 200) {
-         fetch(error_page).then(response => response.text()).then(data => {
-            document.getElementById("error-here").innerHTML += data;
-         });
-      } else if (request.readyState == 4) {
-         // Parse images
-         var resp = request.responseText;
-         const regex = /<img [a-z0-9A-Z=."\/_\-?\s ;:,()\'&\\]*src=\"([a-z0-9A-Z=.\/_\-?;:&\\]*)\"[a-z0-9A-Z=."\/_\-? ;:&]*>/g;
-         var img_arr = [...resp.matchAll(regex)];
-         // Print images
-         for (img of img_arr) {
-            var img_path = img[1];
-            var img_url;
-            if (img_path.slice(0, 2) == "//") {
-               img_url = "https:" + img_path;
-            } else {
-               img_url = domain + img_path;
-            }
-            document.getElementById("cards-here").innerHTML += '<div class="img card"> \
-                  <a class="img" target="_blank" href="' + img_url + '"> \
-                     <img class="img card-img-top" src="' + img_url + '"> \
-                  </a>  \
-                  <div class="img-text card-body row">  \
-                     <p class="img-text card-text col">Аноним</p>  \
-                     <p class="img-text card-text col" style="text-align: end;">№16423301</p> \
-                  </div> \
-               </div>';
+      if (request.status != 200 || request.readyState != 4) {
+         fetch(error_page).then(response => response.text())
+            .then(data => { document.getElementById("error-here").innerHTML += data; });
+         return;
+      }
+      // Parse images
+      var resp = request.responseText;
+      const regex = /<img [a-z0-9A-Z=."\/_\-?\s ;:,()\'&\\]*src=\"([a-z0-9A-Z=.\/_\-?;:&\\]*)\"[a-z0-9A-Z=."\/_\-? ;:&]*>/g;
+      var img_arr = [...resp.matchAll(regex)];
+      // Print images
+      for (img of img_arr) {
+         var img_path = img[1];
+         var img_url;
+         if (img_path.slice(0, 2) == "//") {
+            img_url = "https:" + img_path;
+         } else {
+            img_url = domain + img_path;
          }
+         document.getElementById("cards-here").innerHTML += '<div class="img card"> \
+               <a class="img" target="_blank" href="' + img_url + '"> \
+                  <img class="img card-img-top" src="' + img_url + '"> \
+               </a>  \
+               <div class="img-text card-body row">  \
+                  <p class="img-text card-text col">Аноним</p>  \
+                  <p class="img-text card-text col" style="text-align: end;">№16423301</p> \
+               </div> \
+            </div>';
       }
    }
 }
 
+// Run
 main();
